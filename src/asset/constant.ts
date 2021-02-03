41f2af86bf045d5331a52d65c1b1994ce97a0cc8
박진용 <jinyoung1997@gmail.com>
2021-02-03 13:16:09 +0900

    feat : constant asset added

---
+++ src/asset/constant.ts
@@ -0,0 +1,32 @@
+export const BREAKPOINT = 640;
+
+export const STATIC_URL = {
+  LOGO: `https://user-images.githubusercontent.com/41494099/86317182-c58a8900-bc69-11ea-9a6e-dfdb1e0141be.png`,
+  LOGO_WITHOUT_TEXT: `https://user-images.githubusercontent.com/41494099/87123039-f5b4d600-c2c0-11ea-888c-62d45df7206d.png`,
+  MEMBER_ICON: `https://user-images.githubusercontent.com/41494099/87124999-b9837480-c2c4-11ea-8971-0bc9de8b317d.png`,
+  MAGNIFYING_GLASS_ICON: `https://user-images.githubusercontent.com/41494099/87172994-10626b80-c310-11ea-99d3-bcc2da2cdb6d.png`,
+  Trophy: `https://user-images.githubusercontent.com/60877502/87529160-8af10980-c6c9-11ea-8097-587c3709f810.png`,
+  QUIZ: `https://user-images.githubusercontent.com/60877502/87529727-5f225380-c6ca-11ea-9fe3-e6fa0cf8afee.png`,
+  PLAYBUTTON: `https://user-images.githubusercontent.com/60877502/87544312-3194d480-c6e1-11ea-945e-4a84de3ae581.png`,
+  PROFILE: `https://user-images.githubusercontent.com/60877502/87563353-12ef0780-c6fa-11ea-9c23-4674cb2f3b56.png`,
+  MINIGAME: `https://user-images.githubusercontent.com/60877502/87544826-09f23c00-c6e2-11ea-98b2-1127263e7a82.png`,
+  BACKGROUND: `https://user-images.githubusercontent.com/60877502/87565153-3dda5b00-c6fc-11ea-9497-107cdba965d4.png`,
+  Gold_Medal: `https://user-images.githubusercontent.com/60877502/87629556-3784c700-c76e-11ea-9ea8-0047c0d78b18.png`,
+  Silver_Medal: `https://user-images.githubusercontent.com/60877502/87629596-48353d00-c76e-11ea-89ad-536178858cff.png`,
+  Bronze_Medal: `https://user-images.githubusercontent.com/60877502/87629611-4ec3b480-c76e-11ea-91ec-9a6d7eea3c0a.png`,
+  BACKGROUND_BLACK: `https://user-images.githubusercontent.com/60877502/87670447-b13aa600-c7aa-11ea-91f0-d1956985f288.png`,
+  Level1_Icon: `https://user-images.githubusercontent.com/60877502/87673331-56f01400-c7af-11ea-9ccd-92545061bf62.png`,
+  Level2_Icon: `https://user-images.githubusercontent.com/60877502/87679434-23b18300-c7b7-11ea-9141-856fa74a2b15.png`,
+  Level3_Icon: `https://user-images.githubusercontent.com/60877502/87767649-ecde7a00-c855-11ea-83ba-c44426dc76eb.png`,
+  Level4_Icon: `https://user-images.githubusercontent.com/60877502/87767742-10092980-c856-11ea-8fbe-07b951bde739.png`,
+  Level5_Icon: `https://user-images.githubusercontent.com/60877502/87768110-a0476e80-c856-11ea-8d42-fae61866c846.png`,
+  Coin: `https://user-images.githubusercontent.com/60877502/87688838-73497c00-c7c2-11ea-949b-d0c85fa07097.png`,
+  DataStructure: `https://user-images.githubusercontent.com/60877502/87900458-5c8b7980-ca8f-11ea-851b-793ec15dba92.jpg`,
+};
+
+export const THEME_COLOR = {
+  PRIMARY: "#C2201F",
+  SECONDARY: "#F19296",
+  GRAY: "#F1F1F1",
+  GRAYER: "#EDEDED",
+};
