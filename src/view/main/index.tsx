8b35c0e05757868e0c25f5d68d8eee3f064d751e
박진용 <jinyoung1997@gmail.com>
2021-02-06 16:44:07 +0900

    feat : add main Page

--- src/view/main/index.tsx
+++ src/view/main/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Layout from "../../component/common/layout/index";
 import MenuButtons from "../../component/menu-bottons/index";
 
 const MainPage = () => {
   return (
-    <>
+    <Layout>
       <MenuButtons />
-    </>
+    </Layout>
   );
 };
 
