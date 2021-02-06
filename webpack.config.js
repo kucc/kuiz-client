dbccbabbac4a172a08da3a08f777a2976f91b5b9
박진용 <jinyoung1997@gmail.com>
2021-02-06 21:57:09 +0900

    chore : add bable polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: ["@babel/polyfill", "./src/index.tsx"],
   devtool: "eval-source-map",
   resolve: {
     extensions: [".js", ".ts", ".tsx"],
