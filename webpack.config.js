6c91ac8264180e58ccb06bc98f2ddc2e14f82268
박진용 <jinyoung1997@gmail.com>
2021-02-07 15:53:50 +0900

    chore : alias 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   entry: ["@babel/polyfill", "./src/index.tsx"],
   devtool: "eval-source-map",
   resolve: {
     extensions: [".js", ".ts", ".tsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+      "@asset": path.resolve(__dirname, "src/asset/"),
+      "@common": path.resolve(__dirname, "src/common/"),
+      "@component": path.resolve(__dirname, "src/component/"),
+      "@container": path.resolve(__dirname, "src/container/"),
+      "@view": path.resolve(__dirname, "src/view/"),
+    },
   },
   devServer: {
     historyApiFallback: true,
