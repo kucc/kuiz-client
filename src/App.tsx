5684e00eacc476c21e7f87cde2bc7f2e44c950c2
박진용 <jinyoung1997@gmail.com>
2021-02-09 12:49:33 +0900

    feat : add LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Switch, Redirect, Route } from "react-router-dom";
-import CategoryPage from "./view/category/index";
-import MainPage from "./view/main/index";
-import ProfilePage from "./view/profile/index";
-import RankPage from "./view/rank/index";
+import CategoryPage from "@view/category/index";
+import LoginPage from "@view/login";
+import MainPage from "@view/main/index";
+import ProfilePage from "@view/profile/index";
+import RankPage from "@view/rank/index";
 
 const App = () => {
   return (
     <Switch>
       <Route path="/" exact component={MainPage} />
+      <Route path="/login" component={LoginPage} />
       <Route path="/category" component={CategoryPage} />
       <Route path="/profile" component={ProfilePage} />
       <Route path="/rank" component={RankPage} />
---
+++ src/view/login/index.tsx
@@ -0,0 +1,8 @@
+import Layout from "@component/common/layout";
+import React from "react";
+
+const LoginPage = () => {
+  return <Layout></Layout>;
+};
+
+export default LoginPage;
