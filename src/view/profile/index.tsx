a5ca1d832a5b694a084dd3c3b01fe8e988820b82
easymean <m2162003@gmail.com>
2021-02-15 19:34:38 +0900

    feat: profile 컨테이너 - 프로필

--- src/view/profile/index.tsx
+++ src/view/profile/index.tsx
@@ -1,29 +1,8 @@
-import UserModel from "@/common/model/user";
-import { RootState } from "@/modules";
-import { getUserInfoAsync } from "@/modules/user";
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import ProfileContainer from "../../container/profile-container";
 
 const ProfilePage = () => {
-  const { data, loading, error } = useSelector(
-    (state: RootState) => state.userReducer
-  );
-  const [userData, setUserData] = useState({} as UserModel);
-  const dispatch = useDispatch();
-
-  const getUserINfo = () => {
-    dispatch(getUserInfoAsync.request());
-  };
-
-  useEffect(() => {
-    getUserINfo();
-  }, [dispatch]);
-  return (
-    <>
-      <h1>프로필</h1>
-      {loading ? <h1>로딩중</h1> : <h1>{data?.email}</h1>}
-    </>
-  );
+  return <ProfileContainer />;
 };
 
 export default ProfilePage;
