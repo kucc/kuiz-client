f18c12cab5de4ee41a8cb52142da5952cca93288
박진용 <jinyoung1997@gmail.com>
2021-02-16 01:52:36 +0900

    style : margin 수정

--- src/view/main/styles.ts
+++ src/view/main/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ServiceTitle = styled.div`
-  margin-top: 20rem;
+  margin-top: 18rem;
   font-size: 4.2rem;
   font-weight: bold;
   margin-bottom: 2rem;
