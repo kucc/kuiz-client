1e2649832019eb77592186985b4de809f94e20c4
박진용 <jinyoung1997@gmail.com>
2021-02-15 17:04:27 +0900

    refactor : user reducer 이름 수정

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
 import { all } from "redux-saga/effects";
 import { userSaga } from "./user";
-import userReducer from "./user/reducer";
-const rootReducer = combineReducers({ userReducer });
+import user from "./user/reducer";
+const rootReducer = combineReducers({ user });
 
 export default rootReducer;
 
