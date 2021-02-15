e26af632dcfc5272cf8e8e89140abaeb1afa5d79
박진용 <jinyoung1997@gmail.com>
2021-02-15 20:59:45 +0900

    feat : user 본인 rank 추가

--- src/container/rank-container/index.tsx
+++ src/container/rank-container/index.tsx
@@ -1,25 +1,45 @@
+import userAPI from "@/common/lib/api/user";
 import UserModel from "@/common/model/user";
+import UserWithRankModel from "@/common/model/user-with-rank";
 import RankRow from "@/component/rank-row";
 import React, { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 import { getRankListMokData } from "./mok-datat";
 import * as S from "./styles";
 
 const RankContainer = () => {
   const [rankList, setRankList] = useState([] as UserModel[]);
+  const [userInfoWithRank, setUserInfoWithRank] = useState(
+    {} as UserWithRankModel
+  );
+  const [authCookie] = useCookies(["accessToken"]);
 
-  console.log("ㅅㅂ");
   const getRankList = async () => {
     const rankList = await getRankListMokData();
     setRankList(rankList);
   };
 
+  const getUserInfoWithRank = async () => {
+    if (authCookie) {
+      const userInfoWithRank = await userAPI.getUserInfoWithRank();
+      setUserInfoWithRank(userInfoWithRank);
+    }
+  };
+
   useEffect(() => {
-    console.log(rankList);
     getRankList();
-  }, [rankList]);
+    getUserInfoWithRank();
+  }, []);
 
   return (
     <>
+      <S.SelfRankContainer>
+        <RankRow
+          user={userInfoWithRank}
+          isSelfRank
+          rank={userInfoWithRank.rank}
+        />
+      </S.SelfRankContainer>
       <S.TotalRankContainer>
         {rankList.map((user, idx) => {
           return (
--- src/container/rank-container/styles.ts
+++ src/container/rank-container/styles.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+export const SelfRankContainer = styled.div`
+  margin: 1rem 0rem 3rem 0rem;
+`;
+
 export const TotalRankContainer = styled.div`
   display: flex;
   flex-direction: column;
