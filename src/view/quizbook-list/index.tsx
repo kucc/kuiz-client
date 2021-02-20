b90f56e4b2222f8f412e4987f57f42ab5f93653e
limjisuu <dlawltn1999@naver.com>
2021-02-20 19:25:55 +0900

    fix : delete an unnecessary file

--- src/view/quizbook-list/index.tsx
+++ src/view/quizbook-list/index.tsx
@@ -2,7 +2,6 @@ import QuizBookContainer from "@/container/quizbook-container/index";
 import React from "react";
 import { Link } from "react-router-dom";
 import { RouteComponentProps } from "react-router-dom";
-import * as S from "./styles";
 
 const QuizBookListPage = ({ history, match }: RouteComponentProps) => {
   const categoryId = match.params["categoryId"];
