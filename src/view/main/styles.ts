bcca3a40e683569fa145acbbef818dcabedcca5b
박진용 <jinyoung1997@gmail.com>
2021-02-21 20:09:24 +0900

    style : main page 공백 조절

--- src/view/main/styles.ts
+++ src/view/main/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const ServiceTitle = styled.div`
-  margin-top: 18rem;
-  font-size: 4.2rem;
+  margin-top: 11rem;
+  font-size: 4.4rem;
   font-weight: bold;
   margin-bottom: 2rem;
 `;
