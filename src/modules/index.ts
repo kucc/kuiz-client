350403cf345536c33c35bddd6115f9d8c978656c
박진용 <jinyoung1997@gmail.com>
2021-02-21 21:37:15 +0900

    feat : category saga 및 reducer 추가
    category get action 처리

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -3,14 +3,16 @@ import { all } from "redux-saga/effects";
 import { userSaga } from "./user";
 import user from "./user/reducer";
 import quizbook from "./quiz-book/reducer";
+import category from "./category/reducer";
 import { quizBookSaga } from "./quiz-book";
+import { categorySaga } from "./category";
 
-const rootReducer = combineReducers({ user, quizbook });
+const rootReducer = combineReducers({ user, quizbook, category });
 
 export default rootReducer;
 
 export type RootState = ReturnType<typeof rootReducer>;
 
 export function* rootSaga() {
-  yield all([userSaga(), quizBookSaga()]);
+  yield all([userSaga(), quizBookSaga(), categorySaga()]);
 }
