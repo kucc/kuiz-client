0d1eff0754e82e601e7d9383fbc4213a2a1d2d08
Jihyun <zzzmilky@naver.com>
2021-02-22 15:46:42 +0900

    feat: add quiz page

---
+++ src/view/add-quiz/index.tsx
@@ -0,0 +1,28 @@
+import React, { useCallback, useState } from "react";
+import { STATIC_URL } from "@/asset/constant";
+import Logo from "@component/logo";
+import * as S from "./styles";
+import AddButton from "@component/buttons/next-button/index";
+import QuizCategorySelect from "../../component/option";
+import InputBox from "@component/input-box/index";
+import CheckBox from "@component/checkbox/index";
+import AddQuizList from "@component/add-quiz-list";
+const AddQuizPage = () => {
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <S.TitleContainer>
+          <S.Title>문제</S.Title>
+        </S.TitleContainer>
+
+        <AddQuizList />
+        <S.ButtonContainer>
+          <S.SaveButton>임시저장</S.SaveButton>
+          <S.SubmitButton>제출</S.SubmitButton>
+        </S.ButtonContainer>
+      </S.Container>
+    </S.Wrapper>
+  );
+};
+
+export default AddQuizPage;
