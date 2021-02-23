910c176bbc93b4130d9e0c7ab212bf8dc70613ee
namda-on <namda@bagjin-yong-ui-MacBookPro.local>
2021-02-23 17:06:41 +0900

    refactor : path 명 통일

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { Switch, Redirect, Route } from "react-router-dom";
-
 import CategoryPage from "@view/category/index";
 import LoginPage from "@view/login";
 import MainPage from "@view/main/index";
 import ProfilePage from "@view/profile/index";
 import RankPage from "@view/rank/index";
-import MakeQuizPage from "./view/make-quiz/index";
-import QuizPage from "./view/quiz/index";
+import MakeQuizPage from "@view/make-quiz/index";
+import QuizPage from "@view/quiz/index";
 import QuizResultPage from "@view/quiz-result/index";
-import Layout from "./component/common/layout";
-import QuizBookListPage from "./view/quizbook-list";
-import Auth from "./component/common/auth";
-import QuizBookPage from "./view/quiz-book/index";
+import Layout from "@component/common/layout";
+import QuizBookListPage from "@view/quizbook-list";
+import QuizBookPage from "@view/quiz-book/index";
+import Auth from "@component/common/auth";
 
 const App = () => {
   return (
