4bfbef1616cf61533e835d2aec3d3031c18c2361
swimme <2sy100@likelion.org>
2021-02-25 21:16:39 +0900

    feat: User-QuizBook API 연동

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -4,15 +4,22 @@ import { userSaga } from "./user";
 import user from "./user/reducer";
 import quizbook from "./quiz-book/reducer";
 import category from "./category/reducer";
+import userQuizBook from "./user-quizbook/reducer";
 import { quizBookSaga } from "./quiz-book";
 import { categorySaga } from "./category";
+import { userQuizBookSaga } from "./user-quizbook";
 
-const rootReducer = combineReducers({ user, quizbook, category });
+const rootReducer = combineReducers({
+  user,
+  quizbook,
+  category,
+  userQuizBook,
+});
 
 export default rootReducer;
 
 export type RootState = ReturnType<typeof rootReducer>;
 
 export function* rootSaga() {
-  yield all([userSaga(), quizBookSaga(), categorySaga()]);
+  yield all([userSaga(), quizBookSaga(), categorySaga(), userQuizBookSaga()]);
 }
