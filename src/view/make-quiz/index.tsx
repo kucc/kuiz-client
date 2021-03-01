67e60fe8f8d5b674b78d14cfb6f694dfaee6f58d
namda-on <1112123111@korea.ac.kr>
2021-03-01 18:37:12 +0900

    refactor : make quiz page  props 대신 useParams 함수를 통해 id 사용으로 수정

--- src/view/make-quiz/index.tsx
+++ src/view/make-quiz/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import MakeQuizContainer from "@container/make-quiz-container/index";
-import { RouteComponentProps } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import QuizInputContainer from "@/container/quiz-input-container";
 
-const MakeQuizPage = (props: RouteComponentProps<{quizId, quizBookId}>) => {
-  const quizBookId = props.match.params["quizbookId"];
+interface QuizBookId {
+  quizBookId: string;
+}
+
+const MakeQuizPage = () => {
+  const { quizBookId } = useParams<QuizBookId>();
   if (!parseInt(quizBookId)) throw new Error("잘못된 URL");
 
-  return <QuizInputContainer {...props} quizId={null} quizBookId={quizBookId} />;
+  return <QuizInputContainer quizId={null} quizBookId={parseInt(quizBookId)} />;
 };
 
 export default MakeQuizPage;
