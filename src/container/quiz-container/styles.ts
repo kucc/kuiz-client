2c0da4115bc77c5b6c2122b29891bbe87a644aec
limjisuu <dlawltn1999@naver.com>
2021-03-05 05:03:29 +0900

    style : add white background to quiz container

--- src/container/quiz-container/styles.ts
+++ src/container/quiz-container/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { BREAKPOINT } from "@asset/constant";
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -8,11 +9,16 @@ export const QuizContainer = styled.div`
 `;
 
 export const QuizWrapper = styled.div`
-  width: 100%;
-  margin: 0 auto;
+  width: 90%;
+  background-color: white;
+  border-radius: 4rem;
+  margin-top: 5rem;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   padding: 5rem 0rem;
+  @media only screen and (min-width: ${BREAKPOINT}px) {
+    width: 60%;
+  }
 `;
