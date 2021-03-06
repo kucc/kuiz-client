f43cc18bab2a5933a6d3fb854a51015d8da41a99
namda-on <1112123111@korea.ac.kr>
2021-03-06 16:53:24 +0900

    feat : profile contianer 닉네임 수정 기능 추가

--- src/container/profile-info-container/index.tsx
+++ src/container/profile-info-container/index.tsx
@@ -1,18 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { STATIC_URL } from "@/asset/constant";
 import { RootState } from "@/modules";
 import { getUserInfoAsync } from "@/modules/user";
 import { MAX_LEVEL_POINT } from "./constants";
+import CommonButton from "@component/buttons/common-button";
 import getUserLevelIcon from "@common/lib/get-user-level-icon.ts";
+import UpdateNicknameInput from "@/component/update-nickname-input/index";
+import { updateUserNicknameAsync } from "@/modules/user";
+
 import * as S from "./styles";
 
 const ProfileInfoContainer = () => {
-  const { loading, error, data } = useSelector(
-    (state: RootState) => state.user
+  const [updateNickname, setUpdateNickname] = useState(false);
+  const { data } = useSelector((state: RootState) => state.user);
+  const [userNickname, setUserNickname] = useState<string>(
+    data ? data.name : ""
   );
-
   const dispatch = useDispatch();
+
+  const toggleUpdateNickname = () => {
+    setUpdateNickname(!updateNickname);
+  };
+
+  const nicknameOnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserNickname(e.target.value);
+  };
+  const enterKeyHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      updateUserNickname();
+    }
+  };
+
+  const updateUserNickname = () => {
+    dispatch(updateUserNicknameAsync.request(userNickname));
+    toggleUpdateNickname();
+  };
 
   const getUserINfo = () => {
     dispatch(getUserInfoAsync.request());
@@ -27,7 +49,26 @@ const ProfileInfoContainer = () => {
       <S.UserInfoWrapper>
         <S.UserInfoContainer>
           <S.UserBasicInfoWrapper>
-            <S.UserName>{data.name}</S.UserName>
+            <S.UserNameRow>
+              {updateNickname ? (
+                <>
+                  <UpdateNicknameInput
+                    defaultName={data.name}
+                    onChangeHandler={nicknameOnChangeHandler}
+                    onKeyUP={enterKeyHandler}
+                  />
+                  <CommonButton text={"취소"} onClick={toggleUpdateNickname} />
+                  <CommonButton text={"저장"} onClick={updateUserNickname} />
+                </>
+              ) : (
+                <>
+                  <S.UserName>{data.name}</S.UserName>
+                  <S.UpdateButtonContainer onClick={toggleUpdateNickname}>
+                    <img src={"/src/asset/pencil.svg"}></img>
+                  </S.UpdateButtonContainer>{" "}
+                </>
+              )}
+            </S.UserNameRow>
             <S.UserEmail>{data.email}</S.UserEmail>
           </S.UserBasicInfoWrapper>
           <S.IconContainer>
