c4a4be947b0aa59dbc6d374c06227cc5c88f74a0
Jihyun <zzzmilky@naver.com>
2021-03-12 22:42:48 +0900

    feat: get quiz question

--- src/component/add-quiz-list/index.tsx
+++ src/component/add-quiz-list/index.tsx
@@ -9,18 +9,55 @@ import CheckBox from "@component/checkbox/index";
 import AddButton from "@component/buttons/add-button/index";
 import { QuizBookProps } from "@component/quizbook/types";
 import { RouteComponentProps, useParams } from "react-router-dom";
-import QuizContainer from "@/container/quiz-container/index";
+import QuizQuestion from "@/component/quiz-question/index";
+import QuizCategoryItem from "@component/quiz-category-item/index";
+import QuizCategoryTitle from "@component/quiz-category-title/index";
+import { CategoryListType } from "@container/category-container/types";
+import { getCategoryAsync } from "@/modules/category";
+import { useEffect, useMemo } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "@/modules";
+import quizAPI from "@/common/lib/api/quiz";
+import quizbookAPI from "@/common/lib/api/quizbook";
+import QuizRequestBody from "@/common/model/quiz-request-body";
+import { QuizListType } from "@container/quiz-container/types";
+import QuizModel from "@common/model/quiz";
+import { defaultQuizRequest } from "@container/quiz-input-container/hooks";
+export interface QuizProps {
+  quizBookid: number;
+}
 interface QuizBookId {
   quizBookId: string;
 }
 
-const AddQuizList = () => {
+const AddQuizList = ({ quizBookid }: QuizProps) => {
+  const history = useHistory();
+
   const { quizBookId } = useParams<QuizBookId>();
+  const [Quiz, setQuiz] = useState({} as QuizModel);
+
+  const dispatch = useDispatch();
+
+  const getQuizList = async () => {
+    const quizList = await quizAPI.getAllQuiz(quizBookid);
+
+    setQuiz(quizList[0]);
+  };
+
+  useEffect(() => {
+    getQuizList();
+  }, []);
 
   return (
     <S.Wrapper>
       <S.Container>
         <AddButton link={`/quiz-book/${parseInt(quizBookId)}/makequiz`} />
+      </S.Container>
+      <S.Container>
+        <S.QuizQuestionContainer>
+          <S.Title>문제</S.Title>
+          <QuizQuestion question={Quiz.question} />
+        </S.QuizQuestionContainer>
       </S.Container>
     </S.Wrapper>
   );
--- src/component/add-quiz-list/styles.ts
+++ src/component/add-quiz-list/styles.ts
@@ -23,3 +23,36 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
+export const CategoryContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CategoryWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const QuizQuestionContainer = styled.div`
+  margin-top: 2rem;
+  width: 80%;
+  height: 6rem;
+  display: flex;
+  flex-direction: center;
+  justify-content: center;
+  align-items: center;
+  border: solid 1.5px transparent;
+  border-color: gray;
+  border-style: dashed;
+  border-radius: 0.7rem;
+
+  background-color: ${THEME_COLOR.GRAY};
+`;
+
+export const Title = styled.div`
+  color: blue;
+  width: 5rem;
+  padding-left: 1rem;
+  padding-bottom: 2rem;
+`;
