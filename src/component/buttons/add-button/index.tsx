1acfb6411b9852be79d6f9f827777990b53609b5
Jihyun <zzzmilky@naver.com>
2021-03-13 00:35:14 +0900

    fix: onclick error

--- src/component/buttons/add-button/index.tsx
+++ src/component/buttons/add-button/index.tsx
@@ -5,20 +5,10 @@ import { useHistory } from "react-router-dom";
 import { RouteComponentProps, useParams } from "react-router-dom";
 
 const AddButton = (props: AddButtonProps) => {
-  interface QuizBookId {
-    quizBookId: string;
-  }
-  const history = useHistory();
-
-  const clickURL = () => {
-    const { quizBookId } = useParams<QuizBookId>();
-    history.push(`/quiz-book/${parseInt(quizBookId)}/makequiz`);
-  };
-
   return (
     <S.AddButton>
       <S.AddButtonContainer to={props.link}>
-        <S.ButtonText onClick={clickURL}>+</S.ButtonText>
+        <S.ButtonText>+</S.ButtonText>
       </S.AddButtonContainer>
     </S.AddButton>
   );
