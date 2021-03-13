62f666f4f50b9c6d7dc2777004e4c6d3db645f22
swimme <2sy100@likelion.org>
2021-03-14 04:51:39 +0900

    feat: quizbook/quiz 수정페이지 접근권한 없으면 show modal

--- src/view/add-quiz/index.tsx
+++ src/view/add-quiz/index.tsx
@@ -6,6 +6,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { getQuizListAsync } from "@/modules/quiz";
 import { RootState } from "@/modules";
 import QuizModel from "@/common/model/quiz";
+import { showAlertModal } from "@/modules/modal";
+import CustomAlert from "@/component/custom-alert";
 
 interface QuizBookId {
   quizBookId: string;
@@ -14,31 +16,46 @@ const AddQuizPage = (): ReactElement => {
   const history = useHistory();
   const dispatch = useDispatch();
   const { quizBookId } = useParams<QuizBookId>();
-  const { data } = useSelector((state: RootState) => state.quiz);
+  const { data, error } = useSelector((state: RootState) => state.quiz);
 
   useEffect(() => {
     if (!parseInt(quizBookId)) history.push("/");
-    dispatch(getQuizListAsync.request({ quizBookId: parseInt(quizBookId) }));
+    dispatch(
+      getQuizListAsync.request({
+        quizBookId: parseInt(quizBookId),
+        checkAuth: true,
+      })
+    );
   }, []);
 
+  useEffect(() => {
+    dispatch(showAlertModal("해당 문제집의 수정 및 삭제 권한이 없습니다."));
+  }, [error]);
+
   return (
-    <S.Wrapper>
-      <S.Container>
-        <S.TitleContainer>
-          <S.Title>문제</S.Title>
-        </S.TitleContainer>
+    <>
+      {error ? (
+        <CustomAlert redirect="/" />
+      ) : (
+        <S.Wrapper>
+          <S.Container>
+            <S.TitleContainer>
+              <S.Title>문제</S.Title>
+            </S.TitleContainer>
 
-        <AddQuizList
-          quizList={data as QuizModel[]}
-          quizBookId={parseInt(quizBookId)}
-        />
+            <AddQuizList
+              quizList={data as QuizModel[]}
+              quizBookId={parseInt(quizBookId)}
+            />
 
-        <S.ButtonContainer>
-          <S.SaveButton>임시저장</S.SaveButton>
-          <S.SubmitButton>제출</S.SubmitButton>
-        </S.ButtonContainer>
-      </S.Container>
-    </S.Wrapper>
+            <S.ButtonContainer>
+              <S.SaveButton>임시저장</S.SaveButton>
+              <S.SubmitButton>제출</S.SubmitButton>
+            </S.ButtonContainer>
+          </S.Container>
+        </S.Wrapper>
+      )}
+    </>
   );
 };
 
--- src/view/edit-quiz/index.tsx
+++ src/view/edit-quiz/index.tsx
@@ -3,6 +3,9 @@ import QuizInputContainer from "@/container/quiz-input-container";
 import { useHistory, useParams } from "react-router-dom";
 import QuizModel from "@/common/model/quiz";
 import quizAPI from "@/common/lib/api/quiz";
+import CustomAlert from "@/component/custom-alert";
+import { useDispatch } from "react-redux";
+import { showAlertModal } from "@/modules/modal";
 
 interface QuizId {
   quizId: string;
@@ -10,21 +13,38 @@ interface QuizId {
 
 const EditQuizPage = (): ReactElement => {
   const history = useHistory();
+  const dispatch = useDispatch();
   const { quizId } = useParams<QuizId>();
   const [data, setData] = useState<QuizModel>();
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
     if (!parseInt(quizId)) history.push("/");
 
     const getQuiz = async () => {
-      const quiz = await quizAPI.getQuiz(parseInt(quizId));
-      setData(quiz);
+      const quiz = await quizAPI.getQuiz(parseInt(quizId)).catch(() => {
+        setError(true);
+      });
+      if (quiz) setData(quiz);
     };
     getQuiz();
   }, []);
 
+  useEffect(() => {
+    dispatch(showAlertModal("해당 문제집의 수정 및 삭제 권한이 없습니다."));
+  }, [error]);
+
   return (
-    <QuizInputContainer data={data as QuizModel} quizId={parseInt(quizId)} />
+    <>
+      {error ? (
+        <CustomAlert redirect={"/"} />
+      ) : (
+        <QuizInputContainer
+          data={data as QuizModel}
+          quizId={parseInt(quizId)}
+        />
+      )}
+    </>
   );
 };
 export default EditQuizPage;
