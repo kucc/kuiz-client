cf1b95f212f75c4cdecff5e512fa0eb1e3ac708f
namda-on <1112123111@korea.ac.kr>
2021-03-13 16:21:41 +0900

    feat : user name 글자수 설정

--- src/container/profile-info-container/index.tsx
+++ src/container/profile-info-container/index.tsx
@@ -4,11 +4,13 @@ import { RootState } from "@/modules";
 import { getUserInfoAsync } from "@/modules/user";
 import { MAX_LEVEL_POINT } from "./constants";
 import CommonButton from "@component/buttons/common-button";
-import getUserLevelIcon from "@common/lib/get-user-level-icon.ts";
+import getUserLevelIcon from "@common/lib/get-user-level-icon";
 import UpdateNicknameInput from "@/component/update-nickname-input/index";
 import { updateUserNicknameAsync } from "@/modules/user";
 
 import * as S from "./styles";
+import CustomAlert from "@/component/custom-alert";
+import { showAlertModal } from "@/modules/modal";
 
 const ProfileInfoContainer = () => {
   const [updateNickname, setUpdateNickname] = useState(false);
@@ -32,6 +34,10 @@ const ProfileInfoContainer = () => {
   };
 
   const updateUserNickname = () => {
+    if (userNickname.length >= 10) {
+      dispatch(showAlertModal("유저이름을 10글자 이하로 해주세요"));
+      return;
+    }
     dispatch(updateUserNicknameAsync.request(userNickname));
     toggleUpdateNickname();
   };
@@ -44,57 +50,66 @@ const ProfileInfoContainer = () => {
     getUserINfo();
   }, [dispatch]);
 
-  return data ? (
-    <S.ProfileInfoContainer>
-      <S.UserInfoWrapper>
-        <S.UserInfoContainer>
-          <S.UserBasicInfoWrapper>
-            <S.UserNameRow>
-              {updateNickname ? (
-                <>
-                  <UpdateNicknameInput
-                    defaultName={data.name}
-                    onChangeHandler={nicknameOnChangeHandler}
-                    onKeyUP={enterKeyHandler}
-                  />
-                  <CommonButton text={"취소"} onClick={toggleUpdateNickname} />
-                  <CommonButton text={"저장"} onClick={updateUserNickname} />
-                </>
-              ) : (
-                <>
-                  <S.UserName>{data.name}</S.UserName>
-                  <S.UpdateButtonContainer onClick={toggleUpdateNickname}>
-                    <img src={"/src/asset/pencil.svg"}></img>
-                  </S.UpdateButtonContainer>{" "}
-                </>
-              )}
-            </S.UserNameRow>
-            <S.UserEmail>{data.email}</S.UserEmail>
-          </S.UserBasicInfoWrapper>
-          <S.IconContainer>
-            <S.LevelIcon
-              src={getUserLevelIcon(data.level)}
-              alt="Icon"
-            ></S.LevelIcon>
-          </S.IconContainer>
-        </S.UserInfoContainer>
-      </S.UserInfoWrapper>
-      <S.ProfilePointBarContainer>
-        <S.PointBarWrapper>
-          <S.UserPoint> {data.point} P</S.UserPoint>
-          <S.BackgroundBar>
-            <S.PointBar
-              width={(data.point * 100) / MAX_LEVEL_POINT[data.level]}
-            />
-          </S.BackgroundBar>
-        </S.PointBarWrapper>
-        <S.Point>
-          ( {data.point} / {MAX_LEVEL_POINT[data.level]} )
-        </S.Point>
-      </S.ProfilePointBarContainer>
-    </S.ProfileInfoContainer>
-  ) : (
-    <></>
+  return (
+    data && (
+      <>
+        <S.ProfileInfoContainer>
+          <S.UserInfoWrapper>
+            <S.UserInfoContainer>
+              <S.UserBasicInfoWrapper>
+                <S.UserNameRow>
+                  {updateNickname ? (
+                    <>
+                      <UpdateNicknameInput
+                        defaultName={data.name}
+                        onChangeHandler={nicknameOnChangeHandler}
+                        onKeyUP={enterKeyHandler}
+                      />
+                      <CommonButton
+                        text={"취소"}
+                        onClick={toggleUpdateNickname}
+                      />
+                      <CommonButton
+                        text={"저장"}
+                        onClick={updateUserNickname}
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <S.UserName>{data.name}</S.UserName>
+                      <S.UpdateButtonContainer onClick={toggleUpdateNickname}>
+                        <img src={"/src/asset/pencil.svg"}></img>
+                      </S.UpdateButtonContainer>{" "}
+                    </>
+                  )}
+                </S.UserNameRow>
+                <S.UserEmail>{data.email}</S.UserEmail>
+              </S.UserBasicInfoWrapper>
+              <S.IconContainer>
+                <S.LevelIcon
+                  src={getUserLevelIcon(data.level)}
+                  alt="Icon"
+                ></S.LevelIcon>
+              </S.IconContainer>
+            </S.UserInfoContainer>
+          </S.UserInfoWrapper>
+          <S.ProfilePointBarContainer>
+            <S.PointBarWrapper>
+              <S.UserPoint> {data.point} P</S.UserPoint>
+              <S.BackgroundBar>
+                <S.PointBar
+                  width={(data.point * 100) / MAX_LEVEL_POINT[data.level]}
+                />
+              </S.BackgroundBar>
+            </S.PointBarWrapper>
+            <S.Point>
+              ( {data.point} / {MAX_LEVEL_POINT[data.level]} )
+            </S.Point>
+          </S.ProfilePointBarContainer>
+        </S.ProfileInfoContainer>
+        <CustomAlert />
+      </>
+    )
   );
 };
 
