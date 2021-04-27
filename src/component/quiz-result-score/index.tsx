efa3a717ed72fd92a3a39e6e338d3017255574ec
swimme <2sy100@likelion.org>
2021-04-27 19:39:27 +0900

    fix: quizbook 다 푼 결과화면 ui 수정

--- src/component/quiz-result-score/index.tsx
+++ src/component/quiz-result-score/index.tsx
@@ -3,21 +3,29 @@ import { QuizScoreProps } from "./types";
 import * as S from "./styles";
 
 const QuizScore = (props: QuizScoreProps) => {
+  const { correctQuizCount, totalQuizCount, percentage } = props;
+
   return (
-    <S.ScoreContainer>
-      <svg width="180" height="180" viewBox={"0 0 38 38"}>
-        <S.DefaultCircle cx={19} cy={19} r={15.9154} />
-        <S.ProgressCircle cx={19} cy={19} r={15.9154} {...props} />
-        <S.CircleLabel>
-          <S.Score x="50%" y="50%">
-            {props.percentage}%
-          </S.Score>
-          <S.Text x="50%" y="50%">
-            Correct
-          </S.Text>
-        </S.CircleLabel>
-      </svg>
-    </S.ScoreContainer>
+    <>
+      <S.ScoreText>
+        {totalQuizCount}문제 중 {correctQuizCount}문제를 <br />
+        맞췄습니다 🎉
+      </S.ScoreText>
+      <S.ScoreContainer>
+        <svg width="180" height="180" viewBox={"0 0 38 38"}>
+          <S.DefaultCircle cx={19} cy={19} r={15.9154} />
+          <S.ProgressCircle cx={19} cy={19} r={15.9154} {...props} />
+          <S.CircleLabel>
+            <S.Score x="50%" y="50%">
+              {percentage} %
+            </S.Score>
+            <S.Text x="50%" y="50%">
+              Correct
+            </S.Text>
+          </S.CircleLabel>
+        </svg>
+      </S.ScoreContainer>
+    </>
   );
 };
 
